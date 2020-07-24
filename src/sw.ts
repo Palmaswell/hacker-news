1e7457d2d239dfd7f79425805e8deb9f9f2e9bca
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-07-24 17:11:02 +0200

    feat: include basis service worker functionality

--- src/sw.ts
+++ src/sw.ts
@@ -1,25 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function register(): void {
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then(registration => {
-          console.log(
-            `Service Worker registration succeeded. Scope is ${registration.scope}`,
-          );
-        })
-        .catch(error =>
-          console.warn(`Service Worker registration failed with ${error}`),
+    window.addEventListener('load', async () => {
+      try {
+        const registration = await navigator.serviceWorker.register('/sw.js');
+        console.log(
+          `Service Worker registration succeeded. Scope is ${registration.scope}`,
         );
+      } catch (error) {
+        console.warn(`Service Worker registration failed with ${error}`);
+      }
     });
   }
 }
 
-self.addEventListener('install', event => {
-  (event as any).waitUntil(
+self.addEventListener('install', (event: any) => {
+  event.waitUntil(
     caches
       .open('hacker-news-static-assets')
-      .then(cache => cache.addAll(['/', '/index.html', '/app.js'])),
+      .then(cache =>
+        cache
+          .addAll(['/', '/index.html', '/app.js'])
+          .then(() => (self as any).skipWaiting()),
+      ),
+  );
+});
+
+self.addEventListener('fetch', async (event: any) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }),
   );
 });
